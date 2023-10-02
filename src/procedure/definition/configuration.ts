3f377b4d9b8c4822a9e46516e382468784d58b47
wmxpy <wm@mengw.io>
2023-10-02 01:56:39 -0500

    add procedure driver

--- src/procedure/definition/configuration.ts
+++ src/procedure/definition/configuration.ts
@@ -4,9 +4,42 @@
  * @description Configuration
  */
 
-export type PubProcedureConfiguration = {
+import { Pattern } from "@sudoo/pattern";
+
+export enum PUB_PROCEDURE_TYPE {
+
+    START = "START",
+    DRIVER = "DRIVER",
+    END = "END",
+}
+
+export type PubProcedureConfigurationNext = {
 
     readonly procedureName: string;
 
-    readonly driverName: string;
+    readonly conditions: Record<string, Pattern>;
+
+    readonly mapping: Record<string, string>;
 };
+
+export type PubProcedureConfiguration =
+    | {
+
+        readonly type: PUB_PROCEDURE_TYPE.START;
+    }
+    | {
+
+        readonly type: PUB_PROCEDURE_TYPE.DRIVER;
+        readonly procedureName: string;
+
+        readonly driverName: string;
+
+        readonly parameterPatterns: Record<string, Pattern>;
+        readonly outcomePatterns: Record<string, Pattern>;
+
+        readonly nextProcedures: PubProcedureConfigurationNext[];
+    }
+    | {
+
+        readonly type: PUB_PROCEDURE_TYPE.END;
+    };
