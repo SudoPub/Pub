86765db0bf7c0e3c1c362f4eae5e65789c88282c
wmxpy <wm@mengw.io>
2023-10-03 01:43:32 -0500

    add outomce mapping

--- src/workflow/definition/record.ts
+++ src/workflow/definition/record.ts
@@ -5,14 +5,13 @@
  */
 
 import { PubWorkflowConfiguration } from "./configuration";
+import { PubWorkflowOutcomeMappingElement } from "./outcome-mapping";
 import { PubWorkflowRecordMappingElement } from "./record-mapping";
-
-export type PubWorkflowRecordOutcome = Record<string, any>;
 
 export type PubWorkflowRecord = {
 
     readonly configuration: PubWorkflowConfiguration;
 
     readonly identifierMapping: Record<string, PubWorkflowRecordMappingElement>;
-    readonly outcomeMapping: Record<string, PubWorkflowRecordOutcome>;
+    readonly outcomeMapping: Record<string, PubWorkflowOutcomeMappingElement>;
 };
