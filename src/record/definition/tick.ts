c9f8a3f7b39f0545645bbbf20cf28b125c35445b
wmxpy <wm@mengw.io>
2023-10-08 17:13:09 -0500

    add ticke recrod type executer

--- src/record/definition/tick.ts
+++ src/record/definition/tick.ts
@@ -4,6 +4,8 @@
  * @description Tick
  */
 
+import { PubExecuteParameters } from "../../orchestration/definition/execute";
+
 export type PubWorkflowRecordTick =
     & PubWorkflowRecordTickCommon
     & PubWorkflowRecordTickSwitch;
@@ -11,6 +13,7 @@ export type PubWorkflowRecordTick =
 export enum PUB_WORKFLOW_RECORD_TICK_TYPE {
 
     ESPIAL_TRIGGERED = "ESPIAL_TRIGGERED",
+    EXECUTE_PARAMETERS = "EXECUTE_PARAMETERS",
 }
 
 export type PubWorkflowRecordTickCommon = {
@@ -20,10 +23,17 @@ export type PubWorkflowRecordTickCommon = {
 };
 
 export type PubWorkflowRecordTickSwitch =
-    | PubWorkflowRecordTickEspialTriggered;
+    | PubWorkflowRecordTickEspialTriggered
+    | PubWorkflowRecordTickExecuteParameters;
 
 export type PubWorkflowRecordTickEspialTriggered = {
 
     readonly type: PUB_WORKFLOW_RECORD_TICK_TYPE.ESPIAL_TRIGGERED;
     readonly espial: string;
+};
+
+export type PubWorkflowRecordTickExecuteParameters = {
+
+    readonly type: PUB_WORKFLOW_RECORD_TICK_TYPE.EXECUTE_PARAMETERS;
+    readonly parameters: PubExecuteParameters;
 };
--- src/record/record.ts
+++ src/record/record.ts
@@ -4,7 +4,9 @@
  * @description Record
  */
 
+import { UUIDVersion1 } from "@sudoo/uuid";
 import { PubWorkflowConfiguration } from "../workflow/definition/configuration";
+import { PubWorkflowRecordTick, PubWorkflowRecordTickSwitch } from "./definition/tick";
 
 export class PubRecord {
 
@@ -16,11 +18,37 @@ export class PubRecord {
     }
 
     private readonly _configuration: PubWorkflowConfiguration;
+    private readonly _ticks: PubWorkflowRecordTick[];
 
     private constructor(
         configuration: PubWorkflowConfiguration
     ) {
 
         this._configuration = configuration;
+        this._ticks = [];
+    }
+
+    public get configuration(): PubWorkflowConfiguration {
+        return this._configuration;
+    }
+
+    public get ticks(): PubWorkflowRecordTick[] {
+        return this._ticks;
+    }
+
+    public addTick(
+        tick: PubWorkflowRecordTickSwitch,
+    ): this {
+
+        const enrichedTick: PubWorkflowRecordTick = {
+
+            identifier: UUIDVersion1.generateString(),
+            timestamp: new Date(),
+
+            ...tick,
+        };
+
+        this._ticks.push(enrichedTick);
+        return this;
     }
 }
