ca85f6fd8321c8d705fb3a6a5245ffb090bcc709
wmxpy <wm@mengw.io>
2023-10-08 17:00:53 -0500

    remove espial executer

--- src/orchestration/resource/manager.ts
+++ src/orchestration/resource/manager.ts
@@ -4,7 +4,6 @@
  * @description Manager
  */
 
-import { BaseEspialResolver } from "./espial-resolver";
 import { BaseProcedureResolver } from "./procedure-resolver";
 
 export class OrchestrationResourceManager {
@@ -14,19 +13,11 @@ export class OrchestrationResourceManager {
         return new OrchestrationResourceManager();
     }
 
-    private readonly _espialResolvers: Set<BaseEspialResolver>;
     private readonly _procedureResolvers: Set<BaseProcedureResolver>;
 
     private constructor() {
 
-        this._espialResolvers = new Set<BaseEspialResolver>();
         this._procedureResolvers = new Set<BaseProcedureResolver>();
-    }
-
-    public addEspialResolver(resolver: BaseEspialResolver): this {
-
-        this._espialResolvers.add(resolver);
-        return this;
     }
 
     public addProcedureResolver(resolver: BaseProcedureResolver): this {
