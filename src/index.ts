61e85c53a85957d53c72ef4926681444b3255435
WMXPY <wm@mengw.io>
2023-10-15 16:17:18 -0700

    init exmaples

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,7 @@
 export * from "./execute/executer";
 export * from "./orchestration/orchestrator";
 export * from "./record/record";
+export * from "./workflow/definition/configuration";
+export * from "./workflow/definition/outcome-mapping";
+export * from "./workflow/definition/record-mapping";
 
---
+++ test/example/just-run.ts
@@ -0,0 +1,12 @@
+/**
+ * @author WMXPY
+ * @namespace Example
+ * @description Just Run
+ * @override Example
+ */
+
+import { PubWorkflowConfiguration } from "../../src";
+
+export const justRunExample: PubWorkflowConfiguration = {
+
+};
