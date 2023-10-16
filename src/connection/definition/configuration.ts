9aca082b2ae9d2bc25e5ffaff63035f6fe62e8c0
WMXPY <wm@mengw.io>
2023-10-16 00:05:14 -0700

    add init with triger

--- src/connection/definition/configuration.ts
+++ src/connection/definition/configuration.ts
@@ -8,8 +8,8 @@ export type PubConnectionConfiguration = {
 
     readonly identifier: string;
 
-    readonly triggerProcedure: string;
-    readonly nextProcedure: string;
+    readonly triggerProcedureIdentifier: string;
+    readonly nextProcedureIdentifier: string;
 
     readonly parametersMapping: Record<string, string>;
 };
--- src/record/enrich/init-connection.ts
+++ src/record/enrich/init-connection.ts
@@ -7,16 +7,19 @@
 import { PubConnectionConfiguration } from "../../connection/definition/configuration";
 import { generateIdentifier } from "../../util/identifier";
 import { PubRecordConnectionEnrich, PubRecordConnectionEnrichMap } from "../definition/connection-enrich";
+import { PubRecordProcedureEnrich, PubRecordProcedureEnrichMap } from "../definition/procedure-enrich";
 
 export const recordInitEnrichProcedureMap = (
     connections: PubConnectionConfiguration[],
+    enrichProcedureMap: PubRecordProcedureEnrichMap,
 ): PubRecordConnectionEnrichMap => {
 
     const map: PubRecordConnectionEnrichMap = new Map();
 
     for (const connection of connections) {
 
-        const enrich: PubRecordConnectionEnrich = recordInitEnrichConnection(connection);
+        const enrich: PubRecordConnectionEnrich =
+            recordInitEnrichConnection(connection, enrichProcedureMap);
 
         map.set(enrich.connectionIdentifier, enrich);
     }
@@ -25,13 +28,20 @@ export const recordInitEnrichProcedureMap = (
 
 export const recordInitEnrichConnection = (
     connection: PubConnectionConfiguration,
+    enrichProcedureMap: PubRecordProcedureEnrichMap,
 ): PubRecordConnectionEnrich => {
+
+    const triggerEnrich: PubRecordProcedureEnrich<any> | undefined =
+        enrichProcedureMap.get(connection.triggerProcedureIdentifier);
+
+    const nextEnrich: PubRecordProcedureEnrich<any> | undefined =
+        enrichProcedureMap.get(connection.nextProcedureIdentifier);
 
     return {
 
         connectionIdentifier: connection.identifier,
 
-        triggerWaypoint: generateIdentifier(),
-        nextWaypoint: generateIdentifier(),
+        triggerWaypoint: triggerEnrich ? triggerEnrich.exitWaypoint : generateIdentifier(),
+        nextWaypoint: nextEnrich ? nextEnrich.enterWaypoint : generateIdentifier(),
     };
 };
