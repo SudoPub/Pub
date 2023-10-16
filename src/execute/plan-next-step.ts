831edaccf287bcab7a3fa4f59c4eb72b7bfa883e
WMXPY <wm@mengw.io>
2023-10-15 18:43:08 -0700

    add plan net step

---
+++ src/execute/plan-next-step.ts
@@ -0,0 +1,19 @@
+/**
+ * @author WMXPY
+ * @namespace Execute
+ * @description Plan Next Step
+ */
+
+import { PubRecord } from "../record/record";
+
+export type ExecutePlanNextStepResult = {
+
+    // readonly nextStep: ExecuteStep;
+};
+
+export const executePlanNextStep = (
+    _record: PubRecord,
+): ExecutePlanNextStepResult => {
+
+    return {};
+};
