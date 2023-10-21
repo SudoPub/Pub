e4438b0bd0ceb47bbdac323535f8c576895924fb
WMXPY <wm@mengw.io>
2023-10-20 23:46:20 -0700

    add parse

--- src/record/serialize/deserialize.ts
+++ src/record/serialize/deserialize.ts
@@ -4,9 +4,17 @@
  * @description Deserialize
  */
 
+import { PubSerializedRecord } from "../definition/record";
 import { PubRecord } from "../record";
 
-export const deserializePubRecord = (rawRecord: string): PubRecord => {
+export const deserializePubRecord = (serialized: PubSerializedRecord): PubRecord => {
 
-    return JSON.parse(rawRecord);
+    return serialized as any;
+};
+
+export const parsePubRecord = (rawRecord: string): PubRecord => {
+
+    return deserializePubRecord(
+        JSON.parse(rawRecord),
+    );
 };
