6a61f8ad8e8cb06c6599884a981f3b6b6fcfa0ff
WMXPY <wm@mengw.io>
2023-10-27 19:40:46 -0700

    refactor realization

--- src/record/definition/realization.ts
+++ src/record/definition/realization.ts
@@ -4,13 +4,50 @@
  * @description Realization
  */
 
+import { PUB_PROCEDURE_TYPE } from "../../procedure/definition/configuration";
+
 export type PubRealizationOutcome = Record<string, any>;
 
-export type PubRecordRealizationMap = Map<string, PubRecordRealizationResult>;
+export type PubRecordRealizationMap = Map<string, PubRecordRealization<PUB_PROCEDURE_TYPE>>;
 
-export type PubRecordRealizationRecord = Record<string, PubRecordRealizationResult>;
+export type PubRecordRealizationRecord = Record<string, PubRecordRealization<PUB_PROCEDURE_TYPE>>;
 
-export type PubRecordRealizationResult = {
+export type PubRecordRealization<T extends PUB_PROCEDURE_TYPE> =
+    PubRecordRealizationCommon<T> &
+    PubRecordRealizationSwitch<T>;
 
+export type PubRecordRealizationCommon<T extends PUB_PROCEDURE_TYPE> = {
+
+    readonly procedureIdentifier: string;
+    readonly procedureType: T;
+};
+
+export type PubRecordRealizationSwitch<T extends PUB_PROCEDURE_TYPE> =
+    T extends PUB_PROCEDURE_TYPE.START ? PubRecordRealization_Start :
+    T extends PUB_PROCEDURE_TYPE.END ? PubRecordRealization_End :
+    T extends PUB_PROCEDURE_TYPE.DRIVER ? PubRecordRealization_Driver :
+    T extends PUB_PROCEDURE_TYPE.MAP ? PubRecordRealization_Map :
+    never;
+
+
+export type PubRecordRealization_Start = {
+
+    readonly parameters: PubRealizationOutcome;
+};
+
+export type PubRecordRealization_End = {
+
+    readonly outcome: PubRealizationOutcome;
+};
+
+export type PubRecordRealization_Driver = {
+
+    readonly parameters: PubRealizationOutcome;
+    readonly outcome: PubRealizationOutcome;
+};
+
+export type PubRecordRealization_Map = {
+
+    readonly parameters: PubRealizationOutcome;
     readonly outcome: PubRealizationOutcome;
 };
