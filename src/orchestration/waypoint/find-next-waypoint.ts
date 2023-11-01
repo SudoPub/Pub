90658fbaf37b9652c9714bbfa2c804f947ad20e0
WMXPY <wm@mengw.io>
2023-11-01 00:56:50 -0700

    init find next waypoint

--- src/orchestration/waypoint/find-next-waypoint.ts
+++ src/orchestration/waypoint/find-next-waypoint.ts
@@ -3,3 +3,10 @@
  * @namespace Orchestration_Waypoint
  * @description Find Next Waypoint
  */
+
+import { PubRecord } from "../../record/record";
+
+export const findNextWaypoint = (record: PubRecord): any => {
+
+    return record;
+};
