6092e60159dd5c44726afae6413034124b3e90b8
WMXPY <wm@mengw.io>
2023-11-02 22:49:18 -0700

    init tasks

---
+++ src/orchestration/initialize/initialize-tasks.ts
@@ -0,0 +1,15 @@
+/**
+ * @author WMXPY
+ * @namespace Orchestration_Initialize
+ * @description Initialize Tasks
+ */
+
+import { PubTask } from "../../task/task";
+import { PubWorkflowConfiguration } from "../../workflow/definition/configuration";
+
+export const initializeCreateTasks = (
+    _configuration: PubWorkflowConfiguration,
+): PubTask[] => {
+
+    return [];
+};
---
+++ src/task/task.ts
@@ -0,0 +1,8 @@
+/**
+ * @author WMXPY
+ * @namespace Task
+ * @description Task
+ */
+
+export class PubTask {
+}
