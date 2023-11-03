94609c0762d242f1916b258a500ba068aa3d341c
WMXPY <wm@mengw.io>
2023-11-02 20:01:16 -0700

    Add implementation for findTriggerableProcedures function

--- src/orchestration/procedure/find-triggerable-procedures.ts
+++ src/orchestration/procedure/find-triggerable-procedures.ts
@@ -4,11 +4,33 @@
  * @description Find Triggerable Procedures
  */
 
-import { PUB_PROCEDURE_TYPE } from "../../procedure/definition/configuration";
+import { PUB_PROCEDURE_TYPE, PubProcedureConfiguration } from "../../procedure/definition/configuration";
 import { PubRecordProcedureEnrich } from "../../record/definition/procedure-enrich";
 import { PubRecord } from "../../record/record";
 
-// TODO: implement this
+export const findTriggerableProcedures = (
+    record: PubRecord,
+    waypoint: string,
+): Array<PubProcedureConfiguration<PUB_PROCEDURE_TYPE>> => {
+
+    const procedureEnriches: Array<PubRecordProcedureEnrich<PUB_PROCEDURE_TYPE>> =
+        findTriggerableProcedureEnriches(record, waypoint);
+
+    const procedures: Array<PubProcedureConfiguration<PUB_PROCEDURE_TYPE>> =
+        procedureEnriches
+            .map((procedureEnrich: PubRecordProcedureEnrich<PUB_PROCEDURE_TYPE>) => {
+                return procedureEnrich.procedureIdentifier;
+            })
+            .map((procedureIdentifier: string) => {
+                return record.cachedConfiguration.getProcedureByIdentifier(procedureIdentifier);
+            })
+            .filter((possibleProcedure: PubProcedureConfiguration<PUB_PROCEDURE_TYPE> | null) => {
+                return Boolean(possibleProcedure);
+            }) as Array<PubProcedureConfiguration<PUB_PROCEDURE_TYPE>>;
+
+    return procedures;
+};
+
 export const findTriggerableProcedureEnriches = (
     record: PubRecord,
     waypoint: string,
