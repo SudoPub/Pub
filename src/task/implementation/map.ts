d11ae1916e67a0815adff2b1d8053d4d1b63a34a
WMXPY <wm@mengw.io>
2023-11-03 19:23:00 -0700

    Add PubMapTask class for mapping procedure

--- src/task/implementation/map.ts
+++ src/task/implementation/map.ts
@@ -1,25 +1,25 @@
 /**
  * @author WMXPY
  * @namespace Task_Implementation
- * @description Driver
+ * @description Map
  */
 
 import { PUB_PROCEDURE_TYPE, PubProcedureConfiguration } from "../../procedure/definition/configuration";
 import { PubTaskBase } from "../task-base";
 
-export class PubDriverTask extends PubTaskBase {
+export class PubMapTask extends PubTaskBase {
 
     public static fromProcedure(
-        procedure: PubProcedureConfiguration<PUB_PROCEDURE_TYPE.DRIVER>,
-    ): PubDriverTask {
+        procedure: PubProcedureConfiguration<PUB_PROCEDURE_TYPE.MAP>,
+    ): PubMapTask {
 
-        return new PubDriverTask(procedure);
+        return new PubMapTask(procedure);
     }
 
-    private readonly _procedure: PubProcedureConfiguration<PUB_PROCEDURE_TYPE.DRIVER>;
+    private readonly _procedure: PubProcedureConfiguration<PUB_PROCEDURE_TYPE.MAP>;
 
     protected constructor(
-        procedure: PubProcedureConfiguration<PUB_PROCEDURE_TYPE.DRIVER>,
+        procedure: PubProcedureConfiguration<PUB_PROCEDURE_TYPE.MAP>,
     ) {
 
         super();
