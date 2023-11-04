d1ba0ed037f74c621ab42d55b89368af63da4e6e
WMXPY <wm@mengw.io>
2023-11-03 19:54:05 -0700

    Remove unused code

--- test/e2e/workflow/basic.test.ts
+++ test/e2e/workflow/basic.test.ts
@@ -6,19 +6,10 @@
  */
 
 import { expect } from 'chai';
-import { PubExecuter } from '../../../src';
-import { createMockResourceManager } from '../../mock/resource-manager';
 
 describe('Given (Workflow Basic) Use Case', (): void => {
 
-    const resourceManager = createMockResourceManager();
-
     it('Should be able to init just run workflow', (): void => {
-
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const executer: PubExecuter = PubExecuter.create({
-
-        } as any, resourceManager);
 
         expect(1).to.be.equal(1);
     });
