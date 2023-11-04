02f30d43349ad65aeb18d9e8d81a814e560ded38
WMXPY <wm@mengw.io>
2023-11-04 01:32:50 -0700

    Add integ test and example for many to one

---
+++ test/e2e/many-to-one/execute-in-order.test.ts
@@ -0,0 +1,122 @@
+/**
+ * @author WMXPY
+ * @namespace ManyToOne
+ * @description Execute In Order
+ * @override E2E Test
+ */
+
+import { expect } from 'chai';
+import { createPubAction } from '../../../src/action/create';
+import { PUB_ACTION_TYPE } from '../../../src/action/definition/action';
+import { initializeCreateTaskManager, initializeCreateTasks } from '../../../src/orchestration/initialize/create-tasks';
+import { applyActionOnTaskManager } from '../../../src/task/apply/apply';
+import { PubTaskBase } from '../../../src/task/task-base';
+import { PubTaskManager } from '../../../src/task/task-manager';
+import { PubCachedWorkflowConfiguration } from '../../../src/workflow/cache/configuration';
+import { manyToOneExample } from '../../example/many-to-one';
+import { ExpectTask } from '../../expect/expect-task';
+import { ExpectTaskManager } from '../../expect/expect-task-manager';
+
+describe('Given (Many-To-One Execute In Order) Use Case', (): void => {
+
+    const manyToOneConfiguration: PubCachedWorkflowConfiguration = PubCachedWorkflowConfiguration.fromWorkflowConfiguration(
+        manyToOneExample,
+    );
+
+    it('Should be able to create tasks for one to many configuration', (): void => {
+
+        const tasks: PubTaskBase[] = initializeCreateTasks(manyToOneConfiguration);
+
+        expect(tasks).to.be.lengthOf(4);
+
+        ExpectTask.with(tasks[0]).toBeTask();
+        ExpectTask.with(tasks[1]).toBeTask();
+        ExpectTask.with(tasks[2]).toBeTask();
+        ExpectTask.with(tasks[3]).toBeTask();
+    });
+
+    describe('Iteration - [Execute one by one]', (): void => {
+
+        const taskManager: PubTaskManager = initializeCreateTaskManager(manyToOneConfiguration);
+
+        it('Should be able to get first batch of executable tasks', (): void => {
+
+            ExpectTaskManager.with(taskManager)
+                .hasExecutableTaskLength(3)
+                .forTaskWithProcedureIdentifier("INIT");
+
+            ExpectTaskManager.with(taskManager)
+                .forTaskWithProcedureIdentifier("FIRST")
+                .toBeExecutable();
+            ExpectTaskManager.with(taskManager)
+                .forTaskWithProcedureIdentifier("SECOND")
+                .toBeExecutable();
+            ExpectTaskManager.with(taskManager)
+                .forTaskWithProcedureIdentifier("THIRD")
+                .toBeExecutable();
+        });
+
+        it('Should be able to get second batch of executable tasks', (): void => {
+
+            applyActionOnTaskManager(
+                createPubAction(PUB_ACTION_TYPE.TASK_RESOLVE_SUCCEED, {
+                    taskIdentifier: taskManager
+                        .getTasksByProcedureIdentifier("FIRST")[0]
+                        .taskIdentifier,
+                    output: {},
+                }),
+                taskManager,
+            );
+
+            ExpectTaskManager.with(taskManager)
+                .hasExecutableTaskLength(2);
+
+            ExpectTaskManager.with(taskManager)
+                .forTaskWithProcedureIdentifier("SECOND")
+                .toBeExecutable();
+            ExpectTaskManager.with(taskManager)
+                .forTaskWithProcedureIdentifier("THIRD")
+                .toBeExecutable();
+        });
+
+        it('Should be able to get third batch of executable tasks', (): void => {
+
+            applyActionOnTaskManager(
+                createPubAction(PUB_ACTION_TYPE.TASK_RESOLVE_SUCCEED, {
+                    taskIdentifier: taskManager
+                        .getTasksByProcedureIdentifier("SECOND")[0]
+                        .taskIdentifier,
+                    output: {},
+                }),
+                taskManager,
+            );
+
+            ExpectTaskManager.with(taskManager)
+                .hasExecutableTaskLength(1);
+
+            ExpectTaskManager.with(taskManager)
+                .forTaskWithProcedureIdentifier("THIRD")
+                .toBeExecutable();
+        });
+
+        it('Should be able to get fourth batch of executable tasks', (): void => {
+
+            applyActionOnTaskManager(
+                createPubAction(PUB_ACTION_TYPE.TASK_RESOLVE_SUCCEED, {
+                    taskIdentifier: taskManager
+                        .getTasksByProcedureIdentifier("THIRD")[0]
+                        .taskIdentifier,
+                    output: {},
+                }),
+                taskManager,
+            );
+
+            ExpectTaskManager.with(taskManager)
+                .hasExecutableTaskLength(1);
+
+            ExpectTaskManager.with(taskManager)
+                .forTaskWithProcedureIdentifier("END")
+                .toBeExecutable();
+        });
+    });
+});
---
+++ test/example/many-to-one.ts
@@ -0,0 +1,154 @@
+/**
+ * @author WMXPY
+ * @namespace Example
+ * @description Many To One
+ * @override Example
+ */
+
+import { createStringPattern } from "@sudoo/pattern";
+import { PubWorkflowConfiguration } from "../../src";
+import { PUB_CONNECTION_WAYPOINT_TYPE } from "../../src/connection/definition/configuration";
+import { PUB_PROCEDURE_TYPE, PubProcedureConfiguration } from "../../src/procedure/definition/configuration";
+
+export const manyToOneExampleStartProcedure: PubProcedureConfiguration = {
+
+    identifier: "START",
+    type: PUB_PROCEDURE_TYPE.START,
+
+    payload: {
+        patterns: {
+            hello: createStringPattern(),
+        },
+    },
+};
+
+export const manyToOneExampleEndProcedure: PubProcedureConfiguration = {
+
+    identifier: "END",
+    type: PUB_PROCEDURE_TYPE.END,
+
+    payload: {
+        patterns: {
+            hello: createStringPattern(),
+        },
+    },
+};
+
+export const manyToOneExample: PubWorkflowConfiguration = {
+
+    workflowName: "Many To One",
+    workflowDescription: "Many To One Example for Test",
+
+    startParametersPattern: {},
+    procedures: [
+        manyToOneExampleStartProcedure,
+        {
+            identifier: "FIRST",
+            type: PUB_PROCEDURE_TYPE.DRIVER,
+
+            payload: {
+                driverName: "FIRST",
+
+                parameterPatterns: {},
+                outcomePatterns: {},
+            },
+        },
+        {
+            identifier: "SECOND",
+            type: PUB_PROCEDURE_TYPE.DRIVER,
+
+            payload: {
+                driverName: "SECOND",
+
+                parameterPatterns: {},
+                outcomePatterns: {},
+            },
+        },
+        {
+            identifier: "THIRD",
+            type: PUB_PROCEDURE_TYPE.DRIVER,
+
+            payload: {
+                driverName: "THIRD",
+
+                parameterPatterns: {},
+                outcomePatterns: {},
+            },
+        },
+        manyToOneExampleEndProcedure,
+    ],
+    connections: [
+        {
+            identifier: "START-FIRST",
+
+            triggerProcedureIdentifier: "START",
+            triggerProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_END,
+
+            nextProcedureIdentifier: "FIRST",
+            nextProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_START,
+
+            parametersMapping: {
+            },
+        },
+        {
+            identifier: "START-SECOND",
+
+            triggerProcedureIdentifier: "START",
+            triggerProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_END,
+
+            nextProcedureIdentifier: "SECOND",
+            nextProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_START,
+
+            parametersMapping: {
+            },
+        },
+        {
+            identifier: "START-THIRD",
+
+            triggerProcedureIdentifier: "START",
+            triggerProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_END,
+
+            nextProcedureIdentifier: "THIRD",
+            nextProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_START,
+
+            parametersMapping: {
+            },
+        },
+        {
+            identifier: "FIRST-END",
+
+            triggerProcedureIdentifier: "FIRST",
+            triggerProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_END,
+
+            nextProcedureIdentifier: "END",
+            nextProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_START,
+
+            parametersMapping: {
+            },
+        },
+        {
+            identifier: "SECOND-END",
+
+            triggerProcedureIdentifier: "SECOND",
+            triggerProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_END,
+
+            nextProcedureIdentifier: "END",
+            nextProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_START,
+
+            parametersMapping: {
+            },
+        },
+        {
+            identifier: "THIRD-END",
+
+            triggerProcedureIdentifier: "THIRD",
+            triggerProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_END,
+
+            nextProcedureIdentifier: "END",
+            nextProcedureWaypointType: PUB_CONNECTION_WAYPOINT_TYPE.PROCEDURE_SELF_START,
+
+            parametersMapping: {
+            },
+        },
+    ],
+};
