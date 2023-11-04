408f634ece730c49b6d0675f4ee7f7ed85c97bc1
WMXPY <wm@mengw.io>
2023-11-04 00:13:35 -0700

    Remove ESPIAL_TRIGGERED from PUB_ACTION_TYPE enum and PubActionPayloadSwitch

--- src/action/definition/action.ts
+++ src/action/definition/action.ts
@@ -8,7 +8,6 @@ import { TaskExecuteOutput } from "../../task/definition/task";
 
 export enum PUB_ACTION_TYPE {
 
-    ESPIAL_TRIGGERED = "ESPIAL_TRIGGERED",
     TASK_RESOLVE_SUCCEED = "TASK_RESOLVE_SUCCEED",
 }
 
@@ -23,14 +22,8 @@ export type PubAction<T extends PUB_ACTION_TYPE = PUB_ACTION_TYPE> = {
 };
 
 export type PubActionPayloadSwitch<T extends PUB_ACTION_TYPE> =
-    T extends PUB_ACTION_TYPE.ESPIAL_TRIGGERED ? PubActionEspialTriggered :
     T extends PUB_ACTION_TYPE.TASK_RESOLVE_SUCCEED ? PubAction_TaskResolveSucceed :
     never;
-
-export type PubActionEspialTriggered = {
-
-    readonly espial: string;
-};
 
 export type PubAction_TaskResolveSucceed = {
 
