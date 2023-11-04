7cb0de6d9598f755d39f0c7f1c951ded6e42f1a2
WMXPY <wm@mengw.io>
2023-11-04 00:28:46 -0700

    Delete unused code files

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@
  * @description Index
  */
 
-export * from "./orchestration/orchestrator";
 export * from "./workflow/definition/configuration";
 export * from "./workflow/definition/outcome-mapping";
 export * from "./workflow/definition/record-mapping";
