a701134302aeb3bf859ce5acd1097e7a321dc05b
WMXPY <wm@mengw.io>
2023-11-06 00:21:12 -0800

    Add exports for orchestrator and plan definition

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@
  * @description Index
  */
 
+export * from "./orchestrator/orchestrator";
+export * from "./plan/definition/plan";
 export * from "./workflow/definition/configuration";
 export * from "./workflow/definition/outcome-mapping";
 export * from "./workflow/definition/record-mapping";
---
+++ test/e2e/just-run/orchestrator-full-cycle.test.ts
@@ -0,0 +1,23 @@
+/**
+ * @author WMXPY
+ * @namespace JustRun
+ * @description Orchestrator Full Cycle
+ * @override E2E Test
+ */
+
+import { expect } from 'chai';
+import { PUB_PLAN_TYPE, PubOrchestrator, PubPlan } from '../../../src';
+import { justRunExample } from '../../example/just-run';
+
+describe('Given (Just-Run Orchestrator Full Cycle) Use Case', (): void => {
+
+    const orchestrator: PubOrchestrator =
+        PubOrchestrator.fromWorkflowConfiguration(justRunExample);
+
+    it('Should be able to plan for initial input', (): void => {
+
+        const nextPlans: Array<PubPlan<PUB_PLAN_TYPE>> = orchestrator.planNext();
+
+        expect(nextPlans).to.be.lengthOf(1);
+    });
+});
